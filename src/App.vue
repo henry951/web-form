0bd79cce5be1a8ce3baa19a2f5c7e51a6ae118ef
PuiHangYAU <henkry951@gmail.com>
2022-10-09 11:16:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import SignupForm from './components/SignupForm.vue'
+export default {
+  name: 'App',
+  components: { SignupForm }
+}
+</script>
+
+<template>
+  <SignupForm />
+</template>
